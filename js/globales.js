a38816c6dfe98ea727f3f0fa22744033d05c659e
Batiste Kolenc <bkolenc@hotmail.fr>
2021-11-23 12:04:26 +0100

    Ajout d'une variable globale pour traduire les id des candidats en couleurs

--- js/globales.js
+++ js/globales.js
@@ -1,1 +1,39 @@
-var G_sondages = {}
+var G_sondages = {
+    couleurs:{
+        "id_1":"#FF0059",
+        "id_2":"#2B99FC",
+        "id_3":"#042FA0",
+        "id_4":"#6EF000",
+        "id_5":"#071868",
+        "id_6":"#FF0000",
+        "id_7":"#3f4782", // Eric Zemmour
+        "id_8":"#e32831", // Lutte ouvrière
+        "id_9":"#eb0045", // Montebourg
+        "id_10":"#c0081f",// NPA
+        "id_11":"#CE291D",
+        "id_12":"#F7FA05",
+        "id_13":"#071868",
+        "id_14":"#071868",
+        "id_15":"#00647a", // Jean Lassalle
+        "id_16":"#2B99FC", // Les Patriotes
+        "id_17":"#1d7f85", // UPR
+        "id_18":"#FF8000",
+        "id_19":"#003b5c", // Chrétiens démocrates (via)
+        "id_20":"#6EF000",
+        "id_21":"#003b5c",
+        "id_22":"#003b5c",
+        "id_23":"#003b5c",
+        "id_24":"#FF0059",
+        "id_25":"#FF0059",
+        "id_26":"#003b5c",
+        "id_27":"#FF0059",
+        "id_28":"#000000",
+        "id_29":"#6EF000",
+        "id_30":"#003b5c",
+        "id_31":"#003b5c",
+        "id_32":"#003b5c",
+        "id_33":"gray",
+        "id_34":"#9C0D99"
+    }
+}
+
