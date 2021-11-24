a3ebfa5d4a84b22ccb5f2cf68ed3118a743a648b
Batiste Kolenc <bkolenc@hotmail.fr>
2021-11-24 17:11:08 +0100

    Ajout de deux petites fonctions, convertir couleur hex en rgb et recuperation des initiales des candidats

--- js/boiteAOutils.js
+++ js/boiteAOutils.js
@@ -14,3 +14,30 @@ function inArray(needle,haystack)
     return false;
 }
 
+function hexToRgb(hex) {
+    if(hex != undefined)
+    {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+      return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+      } : null;
+    }
+    else
+    {
+        return { r:0, g:0, b:0}
+    }
+  
+}
+
+function recup_initiales(candidat)
+{
+    var candidat = candidat.replace(/-/g, " ");
+    var split = candidat.split(" ");
+    var initiales = "";
+    split.forEach(function(v, k){
+        initiales += v[0];
+    });
+    return initiales
+}
