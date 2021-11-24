92ccb9baccb523963d2313959d14a10967eaad89
Batiste Kolenc <bkolenc@hotmail.fr>
2021-11-24 19:04:00 +0100

    Fonction de déclaration de tous les events liés à la sélection des candidats

---
+++ js/events.js
@@ -0,0 +1,70 @@
+function click_candidats()
+{
+    // A faire :
+    // - Placer la tooltip à gauche ou à droite en fonction de la position de la souris par rapport au milieu de l'écran
+    // - Styler la popup
+    d3.selectAll(".candidat").on("click", null).on("click", function(e){
+        
+        
+        var id = this.getAttribute("data-id"); 
+        var selection = G_sondages.selection_candidats;
+        tooltip(e.pageX, e.pageY, G_sondages.tables.candidats[id].nom_candidat);
+        if(!inArray(id, selection))
+        {
+            G_sondages.selection_candidats.push(id);
+        }
+        else
+        {
+            G_sondages.selection_candidats = G_sondages.selection_candidats.filter(function(item){
+                return item !== id;
+            });
+        }
+        maj_boutons_candidats();
+    })
+    .on("mousemove", null).on("mousemove", function(e){
+        var id = this.getAttribute("data-id"); 
+        tooltip(e.pageX, e.pageY, G_sondages.tables.candidats[id].nom_candidat);
+    });
+    
+    
+    
+    
+    d3.select("html").on("click mouseover", null).on("click mouseover", function(e){
+        
+        check_si_click_hors_bouton(e);
+        
+    });
+
+    function tooltip(x, y, candidat)
+    {
+        clearTimeout(G_sondages.vrac.tooltip_to);
+        d3.select("#tooltip")
+            .style("display","block")
+            .style("left", x+20+"px")
+            .style("top", y+20+"px")
+            .text(candidat);
+
+        G_sondages.vrac.tooltip_to = setTimeout(function(){
+            d3.select("#tooltip").style("display","none");
+        },1000)
+    }
+
+    function check_si_click_hors_bouton(e)
+    {
+        var classe = e.target.getAttribute("class");
+        if(classe == "img_cont" || classe == "initiales" || classe=="candidat")
+        {
+            // do nothing
+        }
+        else
+        {
+            d3.select("#tooltip").style("display","none");
+        }
+    }
+}
+
+
+
+
+
+
