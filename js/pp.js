ba79548825388d0f5e8ff1ceab21f2d5b8cb2551
Jean-Michel Lahire <jemil75@gmail.com>
2021-11-29 19:43:40 +0100

    Second commit

--- js/pp.js
+++ js/pp.js
@@ -1,19 +1,69 @@
-recup_csv_tour1(function(){ // Fonction Async, attention
-    
-    // On formate les données :
-    formater_donnees();
-    
-    
-    // On sélectionne les candidats s'affichant par défaut en fixant un seuil (en pourcent)
-    candidats_par_defaut(G_sondages.vrac.seuil);
-    afficher_boutons_candidats();
-       // Voici à quoi ressemblent maintenant les données brutes pour former les courbes :
-    console.log(G_sondages);
-    
-    
-    // Tests pour afficher toutes les infos relatives à un point :
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    recup_csv_tour1(function(){ // Fonction Async, attention
+        //Formatage pour écrire les dates en français
+        const localeFr= {
+            "dateTime": "%A, le %e %B %Y, %X",
+            "date": "%d/%m/%Y",
+            "time": "%H:%M:%S",
+            "periods": ["AM", "PM"],
+            "days": ["dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi"],
+            "shortDays": ["dim.", "lun.", "mar.", "mer.", "jeu.", "ven.", "sam."],
+            "months": ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"],
+            "shortMonths": ["janv.", "févr.", "mars", "avr.", "mai", "juin", "juil.", "août", "sept.", "oct.", "nov.", "déc."]};
+        d3.timeFormatDefaultLocale(localeFr);
+
+        // On formate les données :
+        formater_donnees();
+
+
+        // On sélectionne les candidats s'affichant par défaut en fixant un seuil (en pourcent)
+        candidats_par_defaut(G_sondages.vrac.seuil);
+        afficher_boutons_candidats();
+        // Voici à quoi ressemblent maintenant les données brutes pour former les courbes :
+
+        //Reformatage des données et injection dans la classe Poll
+
+        let dictToMap = (dict) => {
+            let myMap=new Map();
+            Object.keys(dict)
+                .forEach( (key) => myMap.set( parseInt(key.replace('id_','')) , dict[key] )   );
+            return myMap;
+        };
+
+
+        let tableCandidats=new Map();
+        Object.keys(G_sondages.tables.candidats)
+            .forEach( (key)=> {
+                let newKey=parseInt(key.replace('id_','')),
+                    newValue=Object.assign({}, G_sondages.tables.candidats[key]);   //Shallow copy
+                newValue.couleur=G_sondages.couleurs[key];
+                newValue.nom=newValue.nom_candidat;
+                newValue.initiales=(newValue.nom)?newValue.nom.match(/[A-Z]/g).join(''):'';
+                delete(newValue.nom_candidat);
+                if (!isNaN(newKey)) tableCandidats.set(newKey,newValue);
+            })
+
+
+
+
+        const MainData=new DataWrapper('mainData')
+                .push(G_sondages.tables.resultats_1)
+                .map(d3.autoType),
+            Graph=new Poll('motherOfPolls')
+                .push('resultats',MainData)
+          //      .push( 'candidats',dictToMap(G_sondages.tables.candidats))
+                .push( 'candidats',tableCandidats)
+           //     .push( 'couleurs',dictToMap(G_sondages.couleurs))
+                .push( 'sondages',dictToMap(G_sondages.tables.sondages))
+                .draw();
+
+
+        // Tests pour afficher toutes les infos relatives à un point :
 //    var point_1 = G_sondages.courbe_par_candidat["id_3"][4],
 //        point_2 = G_sondages.courbe_par_candidat["id_16"][32];
 //    console.log(completer_point("id_3", point_1));
 //    console.log(completer_point("id_16", point_2));
+    });
 });
+
